884663154b263043c8bc9125aa8bf96b537a30b3
Kinteshi1 <stephan.auer2002@gmail.com>
2024-12-20 16:34:50 +0100

    changed form to the material component, implemented bootstrap grid

--- src/app/dashboard/add-data/add-data.component.ts
+++ src/app/dashboard/add-data/add-data.component.ts
@@ -3,29 +3,36 @@ import { SharedModule } from '../../shared/shared.module';
 import { Validators, FormBuilder } from '@angular/forms';
 import { StoreService } from '../../shared/store.service';
 import { BackendService } from '../../shared/backend.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-add-data',
-  standalone: true,  // standalone-Komponente
-  imports: [SharedModule],  // Import der benötigten Module
+  standalone: true,
+  imports: [SharedModule],
   templateUrl: './add-data.component.html',
   styleUrls: ['./add-data.component.css']
 })
 export class AddDataComponent implements OnInit {
-  constructor(private formbuilder: FormBuilder, public storeService: StoreService, private backendService: BackendService) {
-  }
+  constructor(private formbuilder: FormBuilder, public storeService: StoreService, private backendService: BackendService, private snackBar: MatSnackBar) { }
+
   public registrationForm: any;
 
   ngOnInit(): void {
     this.registrationForm = this.formbuilder.group({
       name: ['', [Validators.required]],
       courseId: ['', Validators.required],
-      birthdate: [null, Validators.required]
-    })
+      birthdate: [null, Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      newsletter: [false]
+    });
   }
 
   onSubmit() {
-    if(this.registrationForm.valid) {
+    if (this.registrationForm.valid) {
       this.backendService.addRegistration(this.registrationForm.value, this.storeService.currentPage);
+      this.snackBar.open('Registration successful!', 'Close', {
+        duration: 3000,
+      });
     }
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   exports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ]
 })
 export class SharedModule { }
